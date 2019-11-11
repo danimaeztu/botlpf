ea881f97473dbbeba276d7a8029174e6b8122540
Dani Maeztu <daniel.maeztu@gmail.com>
2019-11-11 21:08:30 +0100

    botlpf code

---
+++ Source/config.py
@@ -0,0 +1,14 @@
+# TODO: Fill the gaps
+
+# Twitter API keys
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# MySQL user and password
+sql_user = ''
+sql_pw = ''
+
+# Keyword arguments by default
+tweet = False
